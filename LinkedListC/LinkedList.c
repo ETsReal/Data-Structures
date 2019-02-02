2a5ca6c967fb6592fb7937d9fb6bcc6e8324b51d
EtsReal <pedrobest.m@gmail.com>
2019-02-02 02:09:46 +0000

    LinkedList with C

---
+++ LinkedListC/LinkedList.c
@@ -0,0 +1,83 @@
+#include <malloc.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+// #include <Node.c> Não se "faz" include de .c's
+
+// O que o gajo (VP) faz é um Node.h
+
+// btw gcc *.c  "compila" os .c todos desta pasta 
+
+typedef struct Node
+{
+    int data;
+    struct Node *next;
+} Node;
+
+typedef struct LinkedList{
+    Node* head;
+} LL;
+
+void add(LL* lista, int data){
+    Node* new_node = malloc(sizeof(Node));
+    new_node -> data = data;
+    if (lista -> head == NULL){
+        lista -> head = new_node;
+    }
+    else{
+        Node* current = lista -> head;
+        while (current -> next != NULL){
+            current = current -> next;
+        }
+        current -> next = new_node;
+    }
+}
+ 
+void printLL(LL* lista){
+    while(lista -> head != NULL){
+        printf("%d \n", lista -> head -> data);
+        lista -> head = lista -> head -> next;
+    }
+}
+
+int shearch(LL* lista, int value){
+    Node* current = lista -> head;
+    while (current != NULL){
+        if (current -> data == value){
+            printf("Value found! \n");
+            return current -> data;
+        }
+        current = current -> next;
+    }
+    printf("Value not found \n");
+    return -1;
+}
+
+bool removeNode(LL* lista, int value){
+    Node* current = lista -> head;
+    if (lista -> head -> data == value){
+        lista -> head = lista -> head -> next;
+        return true;
+    }
+    else{
+        while (current -> next != NULL){
+            if (current -> next -> data == value){
+                current -> next = current -> next -> next;
+                return true;
+            }
+            current = current -> next;
+        }
+    }
+    return false;
+}
+
+
+int main(){
+    LL* linked = malloc(sizeof(LL));
+    linked -> head = NULL;
+    add(linked, 3);
+    add(linked, 5);
+    add(linked, 7);
+    removeNode(linked, 3);
+    printLL(linked);
+}
